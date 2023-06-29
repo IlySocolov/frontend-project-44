c21e1321d86d0f2173d8cd3761b56da50dd580c5
INJIR <mazay3521@mail.ru>
2023-06-29 22:41:09 +0300

    3

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+
+export default function foo(game) {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  let hasError = false;
+  const { task } = game();
+  console.log(task);
+  for (let i = 0; i < 3; i += 1) {
+    const { question, rightAnswer } = game();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer:');
+    if (answer.toString() === rightAnswer.toString()) {
+      console.log('Correct!');
+    } else {
+      hasError = true;
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}.Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(!hasError ? `Congratulations, ${name}!` : `Let's try again, ${name}!`);
+}

2a2c8473ae9633acac9d31fabc7429d4626ebab0
INJIR <mazay3521@mail.ru>
2023-06-29 23:46:39 +0300

    3

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import brainProgress from '../../frontend-project-44/src/games/brain-progression.js';
-import start from '../../frontend-project-44/src/index.js';
+import start from '../../frontend-project-43/src/index.js';
 
 start(brainProgress);
